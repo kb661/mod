8ba5762f1e91447a292a44f373839e2ab5d052e3
kb661 <andrew-rost@mail.ru>
2025-01-26 12:36:12 +0300

    Update mod.js

--- mod.js
+++ mod.js
@@ -11084,7 +11084,7 @@
         search: true,
         kp: false,
         imdb: false,
-        disabled: disable_dbg
+        disabled: disable_dbg && !androidHeaders
       }, {
         name: 'collaps',
         title: 'Collaps',
@@ -11123,7 +11123,7 @@
         search: false,
         kp: true,
         imdb: false,
-        disabled: disable_dbg
+        disabled: disable_dbg && !isAndroid
       }, {
         name: 'fancdn2',
         title: 'FanCDN (ID)',
@@ -11161,7 +11161,7 @@
         search: false,
         kp: true,
         imdb: true,
-        disabled: disable_dbg
+        disabled: disable_dbg && !isAndroid
       }];
       var obj_filter_sources = all_sources.filter(function (s) {
         return !s.disabled;
