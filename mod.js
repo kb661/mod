6c53a79064b0970f64291782437c794244638197
kb661 <andrew-rost@mail.ru>
2025-01-27 13:28:56 +0300

    Update mod.js

--- mod.js
+++ mod.js
@@ -1030,8 +1030,44 @@
       extract.media = [];
       var select_title = '';
       var prefer_http = Lampa.Storage.field('online_mod_prefer_http') === true;
-      var embed = atob('aHR0cHM6Ly9hcGkubGFtcGEuc3RyZWFtL2x1bWV4Lw==');
-      var api_suffix = '/' + encodeURIComponent(btoa(window.location.href));
+      var prefer_mp4 = Lampa.Storage.field('online_mod_prefer_mp4') === true;
+      var prox = component.proxy('lumex');
+      var host = atob('aHR0cHM6Ly9wLmx1bWV4LnNwYWNl');
+      var ref = host + '/';
+      var user_agent = Utils.baseUserAgent();
+      var headers = Lampa.Platform.is('android') ? {
+        'Origin': host,
+        'Referer': ref,
+        'User-Agent': user_agent,
+        'Sec-Fetch-Dest': 'empty',
+        'Sec-Fetch-Mode': 'cors',
+        'Sec-Fetch-Site': 'same-site'
+      } : {};
+      var headers2 = Lampa.Platform.is('android') ? {
+        'Origin': host,
+        'Referer': ref,
+        'User-Agent': user_agent,
+        'Sec-Fetch-Dest': 'empty',
+        'Sec-Fetch-Mode': 'cors',
+        'Sec-Fetch-Site': 'same-site',
+        'Cookie': '',
+        'x-csrf-token': ''
+      } : {};
+      var prox_enc = '';
+
+      if (prox) {
+        prox_enc += 'param/Origin=' + encodeURIComponent(host) + '/';
+        prox_enc += 'param/Referer=' + encodeURIComponent(ref) + '/';
+        prox_enc += 'param/User-Agent=' + encodeURIComponent(user_agent) + '/';
+        prox_enc += 'param/Sec-Fetch-Dest=empty/';
+        prox_enc += 'param/Sec-Fetch-Mode=cors/';
+        prox_enc += 'param/Sec-Fetch-Site=same-site/';
+        prox_enc += 'enc/aXAyNjA2OjQ3MDA6MzAzMTo6NjgxNTo0NmQ5Lw%3D%3D/';
+      }
+
+      var prox_enc2 = prox_enc;
+      var embed = atob('aHR0cHM6Ly9hcGkubHVtZXguc3BhY2Uv');
+      var suffix = atob('Y2xpZW50SWQ9Q1dmS1hMYzFhaklkJmRvbWFpbj1tb3ZpZWxhYi5vbmUmdXJsPW1vdmllbGFiLm9uZQ==');
       var filter_items = {};
       var choice = {
         season: 0,
@@ -1040,20 +1076,57 @@
         voice_id: 0
       };
 
-      function lumex_api(api, callback, error) {
+      function lumex_search(api, callback, error) {
         var error_check = function error_check(a, c) {
-          if (a.status == 404 || a.status == 500 || a.status == 0 && a.statusText !== 'timeout') {
+          if (a.status == 404 || a.status == 0 && a.statusText !== 'timeout') {
             if (callback) callback('');
           } else if (error) error(network.errorDecode(a, c));
         };
 
+        var returnHeaders = true;
+        var prox_enc_cookie = prox_enc;
+
+        if (prox) {
+          prox_enc_cookie += 'cookie_plus/param/Cookie=/';
+          returnHeaders = false;
+        }
+
         var success_check = function success_check(json) {
-          callback(json);
+          var cookie = '';
+
+          if (json && json.headers && json.body) {
+            var cookieHeaders = json.headers['set-cookie'] || null;
+
+            if (cookieHeaders && cookieHeaders.forEach) {
+              var values = {};
+              cookieHeaders.forEach(function (param) {
+                var parts = param.split(';')[0].split('=');
+
+                if (parts[0]) {
+                  if (parts[1] === 'deleted') delete values[parts[0]];else values[parts[0]] = parts[1] || '';
+                }
+              });
+              var cookies = [];
+
+              for (var name in values) {
+                cookies.push(name + '=' + values[name]);
+              }
+
+              cookie = cookies.join('; ');
+            }
+
+            json = typeof json.body === 'string' ? Lampa.Arrays.decodeJson(json.body, {}) : json.body;
+          }
+
+          callback(json, cookie);
         };
 
         network.clear();
         network.timeout(20000);
-        network["native"](api, success_check, error_check);
+        network["native"](component.proxyLink(api, prox, prox_enc_cookie), success_check, error_check, false, {
+          headers: headers,
+          returnHeaders: returnHeaders
+        });
       }
       /**
        * Начать поиск
@@ -1067,22 +1140,26 @@
         select_title = object.search || object.movie.title;
         var error = component.empty.bind(component);
         var found = false;
-        var src = embed;
+        var src = embed + 'content';
 
         if (data && data[0] && data[0].content_type && data[0].id) {
           found = true;
-          src += 'findID/' + encodeURIComponent(data[0].id) + '/' + encodeURIComponent(data[0].content_type.replace(/_/g, '-'));
+          src = Lampa.Utils.addUrlComponent(src, 'contentType=' + encodeURIComponent(data[0].content_type.replace(/_/g, '-')));
+          src = Lampa.Utils.addUrlComponent(src, 'contentId=' + encodeURIComponent(data[0].id));
         } else {
-          var imdb_id = (+kinopoisk_id ? !object.clarification && object.movie.imdb_id : kinopoisk_id) || 'null';
-          var kp_id = +kinopoisk_id ? kinopoisk_id : 'null';
-          src += 'searchId/' + encodeURIComponent(imdb_id) + '/' + encodeURIComponent(kp_id);
+          src = Lampa.Utils.addUrlComponent(src, 'contentType=short');
+          src = Lampa.Utils.addUrlComponent(src, (+kinopoisk_id ? 'kpId=' : 'imdbId=') + encodeURIComponent(kinopoisk_id));
         }
 
-        lumex_api(src + api_suffix, function (json) {
-          if (found && json) success(json);else if (!found && json && json.content_type && json.id) {
-            var src2 = embed + 'findID/' + encodeURIComponent(json.id) + '/' + encodeURIComponent(json.content_type.replace(/_/g, '-'));
-            lumex_api(src2 + api_suffix, function (json) {
-              if (json) success(json);else component.emptyForQuery(select_title);
+        src = Lampa.Utils.addUrlComponent(src, suffix);
+        lumex_search(src, function (json, cookie) {
+          if (json) success(json, cookie);else if (!found && !object.clarification && object.movie.imdb_id && kinopoisk_id != object.movie.imdb_id) {
+            var src2 = embed + 'content';
+            src2 = Lampa.Utils.addUrlComponent(src2, 'contentType=short');
+            src2 = Lampa.Utils.addUrlComponent(src2, 'imdbId=' + encodeURIComponent(object.movie.imdb_id));
+            src2 = Lampa.Utils.addUrlComponent(src2, suffix);
+            lumex_search(src2, function (json, cookie) {
+              if (json) success(json, cookie);else component.emptyForQuery(select_title);
             }, error);
           } else component.emptyForQuery(select_title);
         }, error);
@@ -1142,11 +1219,23 @@
       function success(json, cookie) {
         component.loading(false);
 
-        if (json && json.media && json.media.length) {
+        if (json && json.player && json.player.media && json.player.media.length) {
+          prox_enc2 = prox_enc;
+
+          if (prox) {
+            prox_enc2 += 'param/Cookie=' + encodeURIComponent(cookie) + '/';
+            prox_enc2 += 'param/x-csrf-token=' + encodeURIComponent(json.meta || '') + '/';
+          }
+
+          if (Lampa.Platform.is('android')) {
+            headers2['Cookie'] = cookie;
+            headers2['x-csrf-token'] = json.meta || '';
+          }
+
           var seasons = [];
           var season_num = [];
           var season_count = 0;
-          json.media.forEach(function (media) {
+          json.player.media.forEach(function (media) {
             if (media.episodes) {
               season_count++;
 
@@ -1159,7 +1248,7 @@
               seasons.push({
                 season_id: 1,
                 season_name: 'Сезон 1',
-                episodes: json.media
+                episodes: json.player.media
               });
               season_num.push(1);
             }
@@ -1167,9 +1256,7 @@
           extract = {
             seasons: seasons,
             season_num: season_num,
-            media: json.media,
-            tag_url: json.tag_url || '',
-            vast_msg: json.vast_msg || ''
+            media: json.player.media
           };
           filter();
           append(filtred());
@@ -1287,6 +1374,120 @@
         return filtred;
       }
       /**
+       * Получить потоки
+       * @param {String} str
+       * @returns array
+       */
+
+
+      function extractItems(str, url) {
+        if (!str) return [];
+
+        try {
+          var items = component.parseM3U(str).map(function (item) {
+            var link = item.link || '';
+            if (prefer_mp4) link = link.replace(/(\.mp4):hls:manifest\.m3u8$/i, '$1');
+            var quality = item.height;
+            var alt_quality = link.match(/\b(\d\d\d+)\./);
+
+            if (alt_quality) {
+              var alt_height = parseInt(alt_quality[1]);
+              if (alt_height > quality && alt_height <= 4320) quality = alt_height;
+            }
+
+            return {
+              label: quality ? quality + 'p' : '360p ~ 1080p',
+              quality: quality,
+              file: component.proxyStream(component.fixLink(link, url), 'lumex')
+            };
+          });
+          items.sort(function (a, b) {
+            if (b.quality > a.quality) return 1;
+            if (b.quality < a.quality) return -1;
+            if (b.label > a.label) return 1;
+            if (b.label < a.label) return -1;
+            return 0;
+          });
+          return items;
+        } catch (e) {}
+
+        return [];
+      }
+      /**
+       * Получить поток
+       * @param {*} element
+       */
+
+
+      function parseStream(element, call, error, itemsExtractor, str, url) {
+        var file = '';
+        var quality = false;
+        var items = itemsExtractor(str, url);
+
+        if (items && items.length) {
+          file = items[0].file;
+          quality = {};
+          items.forEach(function (item) {
+            quality[item.label] = item.file;
+          });
+        }
+
+        if (file) {
+          element.stream = file;
+          element.qualitys = quality;
+          call(element);
+        } else error();
+      }
+      /**
+       * Получить поток
+       * @param {*} element
+       */
+
+
+      function getStreamM3U(element, call, error, file) {
+        file = file.replace(/\.mp4:hls:manifest/, '');
+        var hls_file = file.replace(/\/\d\d\d+([^\/]*\.m3u8)$/, '/hls$1');
+        network.clear();
+        network.timeout(5000);
+        network["native"](component.proxyStream(hls_file, 'lumex'), function (str) {
+          parseStream(element, call, error, extractItems, str, hls_file);
+        }, function (a, c) {
+          if (file != hls_file) {
+            network.clear();
+            network.timeout(5000);
+            network["native"](component.proxyStream(file, 'lumex'), function (str) {
+              parseStream(element, call, error, extractItems, str, file);
+            }, function (a, c) {
+              error();
+            }, false, {
+              dataType: 'text'
+            });
+          } else error();
+        }, false, {
+          dataType: 'text'
+        });
+      }
+
+      function parseSubs(tracks) {
+        if (!(tracks && tracks.length)) return false;
+        var subtitles = tracks.filter(function (t) {
+          return t.kind === 'captions';
+        }).map(function (item) {
+          var links = item.src || '';
+          var link = links.split(' or ').filter(function (link) {
+            return link;
+          })[0] || '';
+          link = component.fixLinkProtocol(link, prefer_http);
+          return {
+            label: item.label,
+            url: component.proxyStreamSubs(link, 'lumex')
+          };
+        }).filter(function (s) {
+          return s.url;
+        });
+        return subtitles.length ? subtitles : false;
+      }
+      /**
        * Получить поток
        * @param {*} element
        */
@@ -1295,54 +1496,29 @@
       function getStream(element, call, error) {
         if (element.stream) return call(element);
         if (!element.media.playlist) error();
-        lumex_api(embed + 'tok/' + encodeURIComponent(element.media.playlist) + api_suffix, function (json) {
-          if (json && json.host && json.headers) {
-            var url = component.fixLink(element.media.playlist, json.host);
-            network.clear();
-            network.timeout(10000);
-            network["native"](url, function (json) {
-              var url = component.fixLinkProtocol(json && json.url || '', prefer_http);
+        var url = component.fixLink(element.media.playlist, embed);
+        network.clear();
+        network.timeout(10000);
+        network["native"](component.proxyLink(url, prox, prox_enc2), function (json) {
+          var url = component.fixLinkProtocol(json && json.url || '', prefer_http);
 
-              if (url) {
-                element.subtitles = false ;
-                var file = url;
-                var quality = false;
+          if (url) {
+            element.subtitles = parseSubs(element.media.tracks);
 
-                if (endsWith(url, 'hls.m3u8')) {
-                  var base_url = url.substring(0, url.length - 'hls.m3u8'.length);
-                  var max_quality = element.media.max_quality || 1080;
-                  var quality_list = [1080, 720, 480, 360, 240];
-                  var items = [];
-                  quality_list.forEach(function (q) {
-                    if (q <= max_quality) {
-                      items.push({
-                        label: q + 'p',
-                        quality: q,
-                        file: base_url + q + '.mp4'
-                      });
-                    }
-                  });
+            if (endsWith(url, '.m3u8')) {
+              getStreamM3U(element, call, error, url);
+              return;
+            }
 
-                  if (items && items.length) {
-                    file = items[0].file;
-                    quality = {};
-                    items.forEach(function (item) {
-                      quality[item.label] = item.file;
-                    });
-                  }
-                }
-
-                element.stream = file;
-                element.qualitys = quality;
-                call(element);
-              } else error();
-            }, function (a, c) {
-              error();
-            }, {}, {
-              headers: json.headers
-            });
-          }
-        }, error);
+            element.stream = component.proxyStream(url, 'lumex');
+            element.qualitys = false;
+            call(element);
+          } else error();
+        }, function (a, c) {
+          error();
+        }, {}, {
+          headers: headers2
+        });
       }
       /**
        * Добавить видео
@@ -1382,8 +1558,6 @@
                 url: component.getDefaultQuality(element.qualitys, element.stream),
                 quality: component.renameQualityMap(element.qualitys),
                 subtitles: element.subtitles,
-                vast_url: extract.tag_url,
-                vast_msg: extract.vast_msg,
                 timeline: element.timeline,
                 title: element.season ? element.title : select_title + (element.title == select_title ? '' : ' / ' + element.title)
               };
@@ -1407,8 +1581,6 @@
                           call();
                         });
                       },
-                      vast_url: extract.tag_url,
-                      vast_msg: extract.vast_msg,
                       timeline: elem.timeline,
                       title: elem.title
                     };
